cd660131da2e50d039d74b0218439c48a7b448cc
William Hazard <105560469+williamthazard@users.noreply.github.com>
2022-11-21 13:47:47 -0500

    Update tetrabotis.lua

--- tetrabotis.lua
+++ tetrabotis.lua
@@ -650,56 +650,56 @@ end
 function shnth.bar(n, d)
   if d > 0.2 or d < -0.2 then
       if n==1 then
-        params:set('Tetrabobo_firstattack',util.linlin(-1,1,0.03,2,d))
-        params:set('Tetrabobo_firstrelease',util.linlin(-1,1,0.03,2,d))
+        params:set('Tetrabotis_firstattack',util.linlin(-1,1,0.03,2,d))
+        params:set('Tetrabotis_firstrelease',util.linlin(-1,1,0.03,2,d))
         if firstdoubling then 
-          params:set('Tetrabobo_firstpitch',(params:get('Tetrabobo_firstpitch')*2))
-          Tetrabobo.firstbartrig(params:get('Tetrabobo_firstpitch'))
-          params:set('Tetrabobo_firstpitch',(params:get('Tetrabobo_firstpitch')/2))
+          params:set('Tetrabotis_firstpitch',(params:get('Tetrabotis_firstpitch')*2))
+          Tetrabotis.firstbartrig(params:get('Tetrabotis_firstpitch'))
+          params:set('Tetrabotis_firstpitch',(params:get('Tetrabotis_firstpitch')/2))
         elseif firsthalving then
-          params:set('Tetrabobo_firstpitch',(params:get('Tetrabobo_firstpitch')/2))
-          Tetrabobo.firstbartrig(params:get('Tetrabobo_firstpitch'))
-          params:set('Tetrabobo_firstpitch',(params:get('Tetrabobo_firstpitch')*2))
-        else Tetrabobo.firstbartrig(params:get('Tetrabobo_firstpitch')) 
+          params:set('Tetrabotis_firstpitch',(params:get('Tetrabotis_firstpitch')/2))
+          Tetrabotis.firstbartrig(params:get('Tetrabotis_firstpitch'))
+          params:set('Tetrabotis_firstpitch',(params:get('Tetrabotis_firstpitch')*2))
+        else Tetrabotis.firstbartrig(params:get('Tetrabotis_firstpitch')) 
           end
       elseif n==2 then
-        params:set('Tetrabobo_secondattack',util.linlin(-1,1,0.03,2,d))
-        params:set('Tetrabobo_secondrelease',util.linlin(-1,1,0.03,2,d))
+        params:set('Tetrabotis_secondattack',util.linlin(-1,1,0.03,2,d))
+        params:set('Tetrabotis_secondrelease',util.linlin(-1,1,0.03,2,d))
         if seconddoubling then 
-          params:set('Tetrabobo_secondpitch',(params:get('Tetrabobo_secondpitch')*2))
-          Tetrabobo.secondbartrig(params:get('Tetrabobo_secondpitch'))
-          params:set('Tetrabobo_secondpitch',(params:get('Tetrabobo_secondpitch')/2))
+          params:set('Tetrabotis_secondpitch',(params:get('Tetrabotis_secondpitch')*2))
+          Tetrabotis.secondbartrig(params:get('Tetrabotis_secondpitch'))
+          params:set('Tetrabotis_secondpitch',(params:get('Tetrabotis_secondpitch')/2))
         elseif secondhalving then
-          params:set('Tetrabobo_secondpitch',(params:get('Tetrabobo_secondpitch')/2))
-          Tetrabobo.secondbartrig(params:get('Tetrabobo_secondpitch'))
-          params:set('Tetrabobo_secondpitch',(params:get('Tetrabobo_secondpitch')*2))
-        else Tetrabobo.secondbartrig(params:get('Tetrabobo_secondpitch')) 
+          params:set('Tetrabotis_secondpitch',(params:get('Tetrabotis_secondpitch')/2))
+          Tetrabotis.secondbartrig(params:get('Tetrabotis_secondpitch'))
+          params:set('Tetrabotis_secondpitch',(params:get('Tetrabotis_secondpitch')*2))
+        else Tetrabotis.secondbartrig(params:get('Tetrabotis_secondpitch')) 
           end
       elseif n==3 then
-        params:set('Tetrabobo_thirdattack',util.linlin(-1,1,0.03,2,d))
-        params:set('Tetrabobo_thirdrelease',util.linlin(-1,1,0.03,2,d))
+        params:set('Tetrabotis_thirdattack',util.linlin(-1,1,0.03,2,d))
+        params:set('Tetrabotis_thirdrelease',util.linlin(-1,1,0.03,2,d))
         if thirddoubling then 
-          params:set('Tetrabobo_thirdpitch',(params:get('Tetrabobo_thirdpitch')*2))
-          Tetrabobo.thirdbartrig(params:get('Tetrabobo_thirdpitch'))
-          params:set('Tetrabobo_thirdpitch',(params:get('Tetrabobo_thirdpitch')/2))
+          params:set('Tetrabotis_thirdpitch',(params:get('Tetrabotis_thirdpitch')*2))
+          Tetrabotis.thirdbartrig(params:get('Tetrabotis_thirdpitch'))
+          params:set('Tetrabotis_thirdpitch',(params:get('Tetrabotis_thirdpitch')/2))
         elseif thirdhalving then
-          params:set('Tetrabobo_thirdpitch',(params:get('Tetrabobo_thirdpitch')/2))
-          Tetrabobo.thirdbartrig(params:get('Tetrabobo_thirdpitch'))
-          params:set('Tetrabobo_thirdpitch',(params:get('Tetrabobo_thirdpitch')*2))
-        else Tetrabobo.thirdbartrig(params:get('Tetrabobo_thirdpitch')) 
+          params:set('Tetrabotis_thirdpitch',(params:get('Tetrabotis_thirdpitch')/2))
+          Tetrabotis.thirdbartrig(params:get('Tetrabotis_thirdpitch'))
+          params:set('Tetrabotis_thirdpitch',(params:get('Tetrabotis_thirdpitch')*2))
+        else Tetrabotis.thirdbartrig(params:get('Tetrabotis_thirdpitch')) 
           end
       elseif n==4 then
-        params:set('Tetrabobo_fourthattack',util.linlin(-1,1,0.03,2,d))
-        params:set('Tetrabobo_fourthrelease',util.linlin(-1,1,0.03,2,d))
+        params:set('Tetrabotis_fourthattack',util.linlin(-1,1,0.03,2,d))
+        params:set('Tetrabotis_fourthrelease',util.linlin(-1,1,0.03,2,d))
         if fourthdoubling then 
-          params:set('Tetrabobo_fourthpitch',(params:get('Tetrabobo_fourthpitch')*2))
-          Tetrabobo.fourthbartrig(params:get('Tetrabobo_fourthpitch'))
-          params:set('Tetrabobo_fourthpitch',(params:get('Tetrabobo_fourthpitch')/2))
+          params:set('Tetrabotis_fourthpitch',(params:get('Tetrabotis_fourthpitch')*2))
+          Tetrabotis.fourthbartrig(params:get('Tetrabotis_fourthpitch'))
+          params:set('Tetrabotis_fourthpitch',(params:get('Tetrabotis_fourthpitch')/2))
         elseif fourthhalving then
-          params:set('Tetrabobo_fourthpitch',(params:get('Tetrabobo_fourthpitch')/2))
-          Tetrabobo.fourthbartrig(params:get('Tetrabobo_fourthpitch'))
-          params:set('Tetrabobo_fourthpitch',(params:get('Tetrabobo_fourthpitch')*2))
-        else Tetrabobo.fourthbartrig(params:get('Tetrabobo_fourthpitch')) 
+          params:set('Tetrabotis_fourthpitch',(params:get('Tetrabotis_fourthpitch')/2))
+          Tetrabotis.fourthbartrig(params:get('Tetrabotis_fourthpitch'))
+          params:set('Tetrabotis_fourthpitch',(params:get('Tetrabotis_fourthpitch')*2))
+        else Tetrabotis.fourthbartrig(params:get('Tetrabotis_fourthpitch')) 
           end
     end
   end
